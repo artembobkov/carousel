54a5bdd69689ac4057aa17d3cfd3daa1773b8861
Artem Bobkov <humantorch@mail.ru>
2020-03-18 00:27:34 +0300

    Updates

---
+++ precache-manifest.5093bfb3a172789074d9aa9061da0905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c671466f7930fe59c7962c0dbee13c09",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "3ffa36c9b313b82732c6",
+    "url": "/carousel/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "eb8a00e7f9250c3ee202",
+    "url": "/carousel/static/css/main.07ffb70c.chunk.css"
+  },
+  {
+    "revision": "3ffa36c9b313b82732c6",
+    "url": "/carousel/static/js/2.c26bfe61.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/carousel/static/js/2.c26bfe61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8a00e7f9250c3ee202",
+    "url": "/carousel/static/js/main.26543e96.chunk.js"
+  },
+  {
+    "revision": "48eebd7bac34ed7c6343",
+    "url": "/carousel/static/js/runtime-main.3e6e875f.js"
+  }
+]);
